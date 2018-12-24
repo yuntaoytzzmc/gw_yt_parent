0a97439ccac95ed78c0834870c499c3c0625c853
yuntaoyt <13683308013@163.com>
2018-12-24 16:29:17 +0800

    sss

---
+++ gw_yt_parent/gw_yt_zuul/src/main/java/service/DocumentationConfig.java
@@ -0,0 +1,30 @@
+package service;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+
+    @Override
+    public List<SwaggerResource> get() {
+        List resources = new ArrayList();
+        resources.add(swaggerResource("基础微服务接口", "/Test/v2/api-docs", "1.0"));
+        resources.add(swaggerResource("网管", "/v2/api-docs", "1.0"));
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location, String version) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion(version);
+        return swaggerResource;
+    }
+}
